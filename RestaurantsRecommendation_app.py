fe84e2fc559f2f4081015496b9d703a4e35d6b76
Derek <143875991+GameOver0214@users.noreply.github.com>
2024-09-09 11:49:37 +0800

    Update RestaurantsRecommendation_app.py

--- RestaurantsRecommendation_app.py
+++ RestaurantsRecommendation_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 import streamlit as st
 
+import streamlit as st
+
 # Custom CSS to add background image
 background_image_url = "https://cdn.vox-cdn.com/uploads/chorus_image/image/73039055/Valle_KimberlyMotos__1_of_47__websize__1_.0.jpg"
 
@@ -43,6 +45,7 @@ for restaurant in restaurants:
 
 # Footer
 st.write("Powered by Streamlit")
+
 
 
 
